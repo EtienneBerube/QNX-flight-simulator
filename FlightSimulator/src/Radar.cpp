e1d99770208b867cf9e633a0ce96b46f2e1aa6d9
Dean <deancs1997@gmail.com>
2019-11-03 19:58:30 -0500

    Created function which reads the flights info
    The function creates an array of flights each with their own information for easier manipulation

--- FlightSimulator/src/Radar.cpp
+++ FlightSimulator/src/Radar.cpp
@@ -12,20 +12,46 @@ Radar::Radar() {
 airplanes = new int [TestCase::airplane_schedule.size()];
 
 }
-
+/*
+ * Function which reads the information of planes from the TestCase file
+ * and converts it into an array of flights
+ * for easier manipulations
+ * */
 Radar::getAirplanes(){
 	const int totalNumberOfFlights = TestCase::airplane_schedule.size() / 8;
-	const int NUMBER_OF_VAR = 7;
-	int airplaneSchedules = TestCase::airplane_schedule;
+	const int TOTAL_NUMBER_INFO_PER_PLANE = 7;
+	int airplaneSchedules[] = TestCase::airplane_schedule;
+	const int ID = 0;
+	const int SPEED_X = 1;
+	const int SPEED_Y = 2;
+	const int SPEED_Z = 3;
+	const int POSITION_X = 4;
+	const int POSITION_Y = 5;
+	const int POSITION_Z = 6;
+	const int START_TIME = 7;
+	int id, speedx, speedy,speedz, positionx,positiony, positionz, enterTime;
 
-	for (int flight = 0; flight < totalNumberOfFlights; flight++){
 
-		for (int i = 0; i < airplaneSchedule.size(); i++){
 
+	for (int i, flight = 0; i < airplaneSchedule.size(); i++){
+
+		switch (i % TOTAL_NUMBER_INFO_PER_PLANE){
+			case ID: id = airplaneSchedule[i]; break;
+			case SPEED_X : speedx = airplaneSchedule[i]; break;
+			case SPEED_Y : speedy = airplaneSchedule[i]; break;
+			case SPEED_Z : speedz = airplaneSchedule[i]; break;
+			case POSITION_X : positionx = airplaneSchedule[i]; break;
+			case POSITION_Y : positiony = airplaneSchedule[i]; break;
+			case POSITION_Z : positionz = airplaneSchedule[i]; break;
+			case START_TIME : {
+			enterTime = airplaneSchedule[i];
+			airplanes[flight++] = new Flight (id, speedx, speedy,speedz,positionx,positiony,positionz,enterTime);
+				}break;
+			}
 		}
 
 	}
-}
+
 
 
 Radar::~Radar() {
