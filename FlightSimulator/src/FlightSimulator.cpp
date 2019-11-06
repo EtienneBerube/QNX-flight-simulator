ccef818ca191a433a6e97f1db4d495d186224cd1
Etienne <tiennebrb@gmail.com>
2019-11-06 12:34:37 -0500

    Setup timers

--- FlightSimulator/src/FlightSimulator.cpp
+++ FlightSimulator/src/FlightSimulator.cpp
@@ -4,12 +4,38 @@
 #include <unistd.h>
 
 using namespace std;
+#define MY_PULSE_CODE 0x01
 
 //Function initialization
-void setupComponents();
+void setupCommChannels();
 void setupRadar();
 void setupUserCLI();
 void setupSimulation();
+void setupTimers();
+
+
+//Global stuff
+int radarChannelReceiveID;
+int displayChannelReceiveID;
+int historyChannelReceiveID;
+int simulationChannelReceiveID;
+
+timer_t radar_timer;
+struct sigevent radar_event;
+struct itimerspec radar_itime;
+
+timer_t display_timer;
+struct sigevent display_event;
+struct itimerspec display_itime;
+
+timer_t history_timer;
+struct sigevent history_event;
+struct itimerspec history_itime;
+
+timer_t simulation_timer;
+struct sigevent simulation_event;
+struct itimerspec simulation_itime;
+
 
 void* dummyFunction(void* arg){
 	cout << "thread exec" << endl;
@@ -35,9 +61,27 @@ int main() {
 	return 0;
 }
 
-void setupComponents(){
-	int chid =
-	ChannelCreate(_NTO_CHF_FIXED_PRIORITY);
+void setupCommChannels(){
+	int radarChannelReceiveID = ChannelCreate(_NTO_CHF_FIXED_PRIORITY);
+	int displayChannelReceiveID = ChannelCreate(_NTO_CHF_FIXED_PRIORITY);
+	int historyChannelReceiveID = ChannelCreate(_NTO_CHF_FIXED_PRIORITY);
+	int simulationChannelReceiveID = ChannelCreate(_NTO_CHF_FIXED_PRIORITY);
+}
+
+void setupTimers(){
+
+	//Setting up the Radar
+	radar_event.sigev_notify = SIGEV_PULSE;
+	radar_event.sigev_coid = ConnectAttach(ND_LOCAL_NODE, 0, radarChannelReceiveID, _NTO_SIDE_CHANNEL, 0);
+	radar_event.sigev_priority = getprio(0);
+	radar_event.sigev_code = MY_PULSE_CODE;
+
+	timer_create(CLOCK_REALTIME, &event, &timer_id);
+
+	radar_itime.it_value.tv_sec = 15;
+	radar_itime.it_interval.tv_sec = 15;
+	timer_settime(radar_timer, 0, &itime, NULL);
+
 
 }
 
