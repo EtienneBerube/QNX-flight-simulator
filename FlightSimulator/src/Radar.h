961c51cd5a4040d1920bba0ac2b84d72d7623096
Dean <deancs1997@gmail.com>
2019-11-18 13:01:38 -0500

    Added a function which check the distance between two planes

--- FlightSimulator/src/Radar.h
+++ FlightSimulator/src/Radar.h
@@ -22,6 +22,7 @@ public:
 	void scanAirZone(std::vector<Flight*>); // TODO Add other features
 	std::vector<Flight*> scanFromAPoint(int, int, int, std::vector<Flight*>);
 	void writeLogOfPlaneInAirSpace();
+	void displayPlanesAboutToCrash();
 
 	std::vector<Flight*> getPlanesFlyingTooLow();
 
@@ -30,7 +31,9 @@ public:
 	std::vector<Flight*> flightsInAirSpace;
 	std::vector<Flight*> lostFlights;
 	const int static RADAR_AREA = 100000;
-	const int static MIN_FLYING_ALTITUDE = 1000;
+	const int static MIN_FLYING_ALTITUDE = 15000;
+	const int static MIN_HORIZONTAL_DISTANCE_BETWEEN_PLANES = 3;
+	const int static MIN_VERTICAL_DISTANCE_BETWEEN_PLANES = 1000;
 
 private:
 	AirplaneDB* airplaneDB;
