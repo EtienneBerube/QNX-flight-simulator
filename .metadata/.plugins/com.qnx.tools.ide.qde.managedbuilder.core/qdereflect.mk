f2db17d979a04c9bc197a0b8ebda86d2529e3e75
Dean <deancs1997@gmail.com>
2019-11-20 20:32:00 -0500

    trying to remove these files from git

--- .metadata/.plugins/com.qnx.tools.ide.qde.managedbuilder.core/qdereflect.mk
+++
@@ -1,9 +0,0 @@
-cc_reflect=$(if $(filter $(COMPILER_DRIVER),qcc),\
-		qcc $(COMPILER_SPEC) -E -Wp$(comma)-P -Wp$(comma)-v -Wp$(comma)-dD,\
-		$(CC$(select_compiler)) -E -P -v -dD)
-
-qde-reflect:
-	@echo INCVPATH::$(OS)::$(CPU)::$(VARIANT1)::$(INCVPATH)
-	-@$(cc_reflect) $(SPECS_FILE)
-
-.PHONY: qde-reflect
